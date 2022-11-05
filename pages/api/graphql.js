20bcc9346fe784a7aad518c063d0e0f5771fc1f1
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-11-05 06:26:59 -0300

    Commit inicial do projeto

---
+++ pages/api/graphql.js
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from 'apollo-server-micro'
+import { typeDefs, resolvers } from '../../src/models/all'
+
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req, res) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
---
+++ pages/index.js
@@ -0,0 +1,71 @@
+import useSWR from 'swr'
+import React, { useState } from 'react'
+import { fetcher2 } from '../src/utils/defaults'
+import { mutation } from '../src/utils/mutation'
+
+
+const url = '/api/graphql'
+const query = `
+  query {
+    getImoveis {
+      endereco
+      descricao
+      valorVenda
+      valorLocacao
+  }
+}
+`
+
+const Index = () => {
+  const { data: imoveis, error, isValidating, mutate } = useSWR(query, fetcher2)
+
+  const [imovel, setImovel] = useState({
+    "endereco": "",
+    "descricao": "",
+    "valorVenda": 0.01,     // faz a coerção do tipo, API GraphQL fortemente tipada
+    "valorLocacao": 0.01
+  })
+
+  const onChange = (e) => {
+    if (e.target.type === 'number') {
+      // fixa em 2 casas decimais o valor do campo, API GraphQL fortemente tipada
+      setImovel({...imovel, [e.target.name]: parseFloat(e.target.value) + 0.01, })
+      return;
+    }
+
+    setImovel({...imovel, [e.target.name]: e.target.value, })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    mutation(`createImovel`, imovel)
+    .then( (response) => {
+      mutate() // atualiza a lista de imóveis
+    })
+    .catch((error) => console.error(error))
+  }
+
+  return (
+    <>
+      <h1>GraphQL</h1>
+      <pre>{JSON.stringify(imoveis, null, 2)}</pre>
+
+      <form>
+        <fieldset style={{"display": "flex", "flexDirection": "column", "width": "25rem"}}>
+          <label htmlFor="endereco">Endereço</label>
+          <input type="text" name="endereco" onChange={onChange} />
+          <label htmlFor="descricao">Descricao</label>
+          <input type="text" name="descricao" onChange={onChange} />
+          <label htmlFor="valorVenda">Valor de Venda</label>
+          <input type="number" step="0.01" name="valorVenda" onChange={onChange} />
+          <label htmlFor="valorLocacao">Valor de Locação</label>
+          <input type="number" step="0.01" name="valorLocacao" onChange={onChange} />
+        </fieldset>
+        <button type="submit" onClick={handleSubmit} >Salvar</button>
+      </form>
+    </>
+  )
+}
+
+export default Index
