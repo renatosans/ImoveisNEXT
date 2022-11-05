311981b662f583ec49f6bdfbec668330d5f1fbb2
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-11-05 06:28:35 -0300

    Commit inicial do projeto

---
+++ src/database/insert_imovel.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO imoveis.imovel (id, endereco, decricao, valorVenda, valorLocacao) VALUES (1, 'Vila Belmiro, Santos - SP', 'Casa de 3 quartos na vila Belmiro em Santos', 895000.00, NULL);
+INSERT INTO imoveis.imovel (id, endereco, decricao, valorVenda, valorLocacao) VALUES (2, 'Ponta da Praia, Santos - SP', 'Apartamento à venda no bairro Ponta da Praia', 715000.00, NULL);
+INSERT INTO imoveis.imovel (id, endereco, decricao, valorVenda, valorLocacao) VALUES (3, 'Gonzaga, Santos - SP', 'Apartamento Duplex à venda no bairro Gonzaga', 1280000.00, NULL);
+INSERT INTO imoveis.imovel (id, endereco, decricao, valorVenda, valorLocacao) VALUES (4, 'Embaré, Santos - SP', 'Apartamento à venda no bairro Embaré', 299999.99, NULL);
+INSERT INTO imoveis.imovel (id, endereco, decricao, valorVenda, valorLocacao) VALUES (5, 'Aparecida, Santos - SP', 'Casa Normal à venda no bairro Aparecida', 590000.00, NULL);
